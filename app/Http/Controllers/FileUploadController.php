92d15024f8627d07aa90e3d86d38055b5fd92328
tunchamroeun <chamroeuntun25@outlook.com>
2019-12-02 17:22:36 +0700

    mcu backend update file upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,30 +10,30 @@ class FileUploadController extends Controller
 {
     public function upload(Request $request){
         $file = $request->file('file');
-        $img = Image::make($file)->encode('png',10);
-        $name = uniqid().'-'.time() . '.png';
+        $img = Image::make($file)->encode('jpg',75);
+        $name = uniqid().'-'.time() . '.jpg';
         Storage::disk('public')->put($name, $img);
         return Storage::url($name);
     }
     public function upload_original(Request $request){
         $file = $request->file('file');
-        $img = Image::make($file)->encode('png',10);
-        $name = uniqid().'-'.time() . '.png';
+        $img = Image::make($file)->encode('jpg',75);
+        $name = uniqid().'-'.time() . '.jpg';
         Storage::disk('public')->put($name, $img);
         return response()->json(['path'=>Storage::url($name)]);
     }
     public function upload_tinymce(Request $request){
         $file = $request->file('file');
-        $img = Image::make($file)->encode('png',10);
-        $name = uniqid().'-'.time() . '.png';
+        $img = Image::make($file)->encode('jpg',75);
+        $name = uniqid().'-'.time() . '.jpg';
         Storage::disk('public')->put($name, $img);
         return response()->json(['location'=>url(Storage::url($name))]);
     }
     //thumb
     public function upload_thumb(Request $request){
         $file = $request->file('file');
-        $img = Image::make($file)->fit(400, 400)->encode('png',10);
-        $name = uniqid().'-'.time() . '.png';
+        $img = Image::make($file)->fit(400, 400)->encode('jpg',75);
+        $name = uniqid().'-'.time() . '.jpg';
         Storage::disk('public')->put($name, $img);
         return response()->json(['path'=>Storage::url($name)]);
     }
